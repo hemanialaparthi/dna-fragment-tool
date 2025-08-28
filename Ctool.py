96ac841701b56b4e010ccc34c1bc4e3db1ec392f
Coltin Colucci <Colucci01@allegheny.edu>
2025-08-28 14:09:04 -0400

    comments

--- Ctool.py
+++ Ctool.py
@@ -2,17 +2,18 @@
 
 def align_sequences(sequences):
     """Aligns a list of sequences and prints them with mismatches highlighted."""
-    ref = sequences[0]
-    aligned = [list(ref)]
+    ref = sequences[0]      # Use first sequence as reference
+    aligned = [list(ref)]   # Start aligned list with reference sequence
 
     for seq in sequences[1:]:
         aligned_seq = []
-        for r, s in zip(ref, seq.ljust(len(ref))):  # pad shorter seq
+        # compare each character in the reference to the current sequence
+        for r, s in zip(ref, seq.ljust(len(ref))):      # add spaces to shorter seq
             if r == s:
-                aligned_seq.append(s)
+                aligned_seq.append(s)    # match keep character
             else:
-                aligned_seq.append(" ")
-        aligned.append(aligned_seq)
+                aligned_seq.append(" ")  # mismatch insert space
+        aligned.append(aligned_seq)      # add aligned sequence to list
 
     # Print results
     for seq in aligned:
